da6e60259c72d98869e795f09ec8582fff2212ab
shanheng43 <291928159@qq.com>
2020-03-07 18:12:49 +0800

    modify

---
+++ 002/js/alert.js
@@ -0,0 +1,12 @@
+function taolu(){
+    alert("猜猜我是谁？");
+    alert("你再猜猜？");
+    alert("猜不到吧？");
+    alert("就不告诉你")
+}
+alert("你为什么打开这个网站？");
+taolu();
+taolu();
+alert("再等等，我有话对你说");
+alert("也许一会儿会有奇迹呢");
+alert("我爱你");
